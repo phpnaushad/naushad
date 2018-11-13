53347416c5effb2cbfc811e94b001c1bab78a0f1
Naushad Ahmad <naushadalevel@gmail.com>
2018-11-13 17:46:03 +0530

    Update template-blog.php

--- template-blog.php
+++ template-blog.php
@@ -1,61 +1,68 @@
 <?php
-/*
-Template Name:  Blog Page Content
-*/
-
-get_header(); ?>
-<div class="mt_manacourleft">
-  <hgroup class="mtbloghd">
-   <h3>management Blog</h3>
-  </hgroup>
-   
-    <?php query_posts('posts_per_page=5'); ?>
-<?php if (have_posts()) : ?><?php while (have_posts()) : the_post(); 
-$src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 300,300 ), false, '' );
-$thumbnailSrc = $src[0];
-?>
-
-
-
-    <div class="mt_blog">
-   <h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-   <i><?php the_time('F j, Y'); ?></i>
-   <?php echo do_shortcode('[ssboost] '); ?>
-<div class="clear"></div>   
-    <div class="mt_blosection">
-    <img src="<?php bloginfo('template_directory'); ?>/thumb.php?src=<?php echo $thumbnailSrc; ?>&h=200&w=300&zc=1q=100" alt="">
-      
-     <p><?php the_excerpt(); ?></p>
-     <div class="mt_learn mt_sec_btn"><a href="<?php the_permalink(); ?>">Read More</a></div>    
-  
-    <div class="clear"></div>
+/**
+ * Template Name: Blog Page
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+ get_header(); ?>    
+    <div class="inner-banner-section">
+        <div class="container">
+            <div class="row">
+                <div class="page-title col-sm-12">
+                    <h1>Blog</h1>
+                    <!---<p class="page-subtitle">Money Doesn’t Come Without Guidence...</p>--->
+                </div>
+            </div>
+        </div>
     </div>
-    
-   <div class="clear"></div>  
+
+    <div id="main" class="middle-wrapper full-width page-gapping">
+        <div class="container">
+            <div class="row">
+				
+				
+                <div class="site-content col-md-9 col-sm-9 col-xs-9">
+				<?php
+                    $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+                    $args = array('post_type' => 'post', 'posts_per_page' => 2, 'paged' => $paged );
+
+                    $post_type_data = new WP_Query($args);
+
+                    set_query_var('page',$paged);
+                    while ($post_type_data->have_posts()): $post_type_data->the_post();
+                    global $more;
+                        $more = 0; ?>
+                        <div class="blog-item">
+                            
+                            <div class="feature-image img-overlay">
+                                <?php if (has_post_thumbnail()): ?>
+                                    <?php $default = array('class' => 'img-responsive');
+                                    the_post_thumbnail('wl_blog_img', $default); ?>
+                                <?php endif; ?>
+                            </div>
+                            
+                            <div class="feature-content">
+                                <h3 class="h3-blog-title">
+                                    <a href="<?php echo the_permalink(); ?>"><?php echo the_title(); ?></a>
+                                </h3>
+                                <span><i class="icon-picture"></i></span>
+                                <span><i class="icon-time"></i><?php echo get_the_date('j'); ?> <?php echo the_time('M'); ?>, <?php echo the_time('Y'); ?></span>                              
+
+                                <p><?php the_excerpt(); ?></p>
+                                <a class="btn" href="<?php echo the_permalink(); ?>">Read More</a>
+                            </div>
+
+                        </div>
+                    <?php endwhile; ?>
+					<div class="nav-previous alignleft"><?php previous_posts_link('&laquo; Newer posts');?></div>
+                        <div class="nav-next alignright"><?php next_posts_link( 'Older posts &raquo;', $post_type_data->max_num_pages ); ?></div>
+                    
+                </div>	
+                <?php get_sidebar(); ?>
+            </div>
+        </div>
     </div>
-    
-    
-<?php endwhile; ?>
-
-<?php endif; ?>
-    
-      
-<div class="clear"></div>
-</div>
-
-<div class="mt_rightpanl">
-<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
-		<div id="secondary" class="widget-area" role="complementary">
-			<?php dynamic_sidebar( 'sidebar-1' ); ?>
-		</div><!-- #secondary -->
-	<?php endif; ?>
-    
-<div class="clear"></div>
-</div>
-
-
-<div class="clear"></div>
-</div>
-
-
-<?php get_footer(); ?>
+<?php get_footer();
